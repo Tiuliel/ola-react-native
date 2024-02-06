2b352522ce7f8e36af5941fbbae94f5aee01c0c4
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-02-06 11:07:08 -0300

    .

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá React Native e Expo! 😉</Text>
+      <Text>Olá React Native e Expo! 😉 </Text>
       <StatusBar style="auto" />
     </View>
   );
