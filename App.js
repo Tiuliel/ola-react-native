e52c77e5d5759f6fae8586eeff14460d6b9b74f6
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-02-07 11:12:00 -0300

    layout com flex

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   Button,
   SafeAreaView,
   StatusBar,
+  StyleSheet,
   Text,
   View,
 } from "react-native";
@@ -14,17 +15,17 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
+      <SafeAreaView style={estilos.container}>
         <View>
           <Text>Olá React Native!</Text>
         </View>
-        <View>
+        <View style={estilos.cabecalho}>
           <Text>Topo/Cabeçalho</Text>
         </View>
-        <View>
+        <View style={estilos.conteudo}>
           <Text>Conteúdo</Text>
         </View>
-        <View>
+        <View style={estilos.rodape}>
           <Text>Rodapé</Text>
           <Button title="Toca Aqui!!!" onPress={vai} />
         </View>
@@ -32,3 +33,26 @@ export default function App() {
     </>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    flex: 1,
+    /*  justifyContent: "center",
+    flexDirection: "row-reverse", */
+  },
+  cabecalho: {
+    backgroundColor: "lightgreen",
+    flex: 0.7,
+  },
+
+  conteudo: {
+    backgroundColor: "yellow",
+    flex: 4,
+  },
+
+  rodape: {
+    backgroundColor: "orange",
+    flex: 0.5,
+  },
+});
