c4aadc89ecceb2aa2fd02eb93327bd810e9df120
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-02-07 10:30:22 -0300

    status bar

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { StatusBar, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const vai = () => {
+    Alert.alert("Titulo da jenela", "Fala parça!");
+  };
   return (
     <>
       <StatusBar barStyle="light-content" />
-
-      <View>
-        <Text>Olá React Native!</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Olá React Native!</Text>
+        </View>
+        <View>
+          <Text>Topo/Cabeçalho</Text>
+        </View>
+        <View>
+          <Text>Conteúdo</Text>
+        </View>
+        <View>
+          <Text>Rodapé</Text>
+          <Button title="Toca Aqui!!!" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
